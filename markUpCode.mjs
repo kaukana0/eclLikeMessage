176a67ca311cd9eb6d601e92f0418d5316ce9127
robert <kaukana0@tuta.io>
2023-08-23 12:53:52 +0200

    first commit

---
+++ markUpCode.mjs
@@ -0,0 +1,63 @@
+/*
+all HTML and CSS as JS string
+*/
+
+export default class MarkUpCode {
+
+	// helper
+	static getHtmlTemplate(source) {
+		const t = document.createElement('template')
+		t.innerHTML = source
+		return t.content
+	}
+
+  static html() {
+    return `
+    <link  href="../../../redist/ecl/ecl-eu.css" rel="stylesheet"/>
+
+
+
+<div class="ecl-message ecl-message--info" data-ecl-message="" role="alert" data-ecl-auto-init="Message" id="main" style="
+	display:none;
+	position: fixed;
+	top: 50%;
+	left: 50%;
+	transform: translate(-50%, -50%);
+	z-index:10;">
+
+  <span style="display:flex;">
+
+    <svg class="ecl-icon ecl-icon--m ecl-message__icon" focusable="false" aria-hidden="true" >
+      <use xlink:href="../redist/ecl/icons.svg#information"></use>
+    </svg>
+    <div class="ecl-message__content">
+      <button class="ecl-button ecl-button--ghost ecl-message__close" type="button" data-ecl-message-close id="button">
+        <span class="ecl-button__container">
+          <span class="ecl-button__label" data-ecl-label="true">Close</span>
+          <svg class="ecl-icon ecl-icon--s ecl-button__icon ecl-button__icon--after" focusable="false" aria-hidden="true" data-ecl-icon>
+            <use xlink:href="../redist/ecl/icons.svg#close-filled"></use>
+          </svg>
+        </span>
+      </button>
+      <div class="ecl-message__title" id="header">Information message</div>
+      <div class="ecl-message__description" id="text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam accumsan semper lorem, ac mollis lacus tincidunt eu. Duis scelerisque diam eu tempus fringilla.</div>
+    </div>
+
+  <span>
+
+  </div>
+
+
+    `
+  }
+
+ 
+
+  static css() {
+    return `
+    <style>
+    </style>   
+    `
+  }
+
+}
---
+++ message.mjs
@@ -0,0 +1,32 @@
+import MarkUpCode from  "./markUpCode.mjs"		// keep this file html/css free
+
+
+class Element extends HTMLElement {
+	constructor() {	
+		super()	
+		this.attachShadow({mode: 'open'})
+		const tmp = MarkUpCode.getHtmlTemplate(MarkUpCode.html()).cloneNode(true)
+		this.shadowRoot.appendChild(tmp)
+		this.shadowRoot.getElementById("button").addEventListener("click", e=> {
+			this.hide()
+		})
+	}
+
+	setText(val) {
+		this.shadowRoot.getElementById("text").innerHTML=val
+	}
+	setHeader(val) {
+		this.shadowRoot.getElementById("header").innerHTML=val
+	}
+	show() {
+		this.shadowRoot.getElementById("main").style.display="block"
+		setTimeout(()=>this.hide(), 7000)
+	}
+	hide() {
+		this.shadowRoot.getElementById("main").style.display="none"
+	}
+
+
+}
+
+window.customElements.define('ecl-like-message', Element)
