cc2b1657fb3689f334275fbf334cc16073ad21bc
robert <kaukana0@tuta.io>
2024-01-09 15:41:04 +0100

    a11y

--- message.mjs
+++ message.mjs
@@ -7,9 +7,9 @@ class Element extends HTMLElement {
 		this.attachShadow({mode: 'open'})
 		const tmp = MarkUpCode.getHtmlTemplate(MarkUpCode.html()).cloneNode(true)
 		this.shadowRoot.appendChild(tmp)
-		this.shadowRoot.getElementById("button").addEventListener("click", e=> {
-			this.hide()
-		})
+
+		this.shadowRoot.getElementById("close").addEventListener("click", e=> {this.hide()})
+		this.shadowRoot.getElementById("close").addEventListener("keydown", e=> {if(e.key=="Enter" || e.key=="Escape") {this.hide()}} )
 	}
 
 	setText(val) {
@@ -23,6 +23,7 @@ class Element extends HTMLElement {
 		this.shadowRoot.getElementById("bg").style.display="block"
 		this.shadowRoot.getElementById("main").style.display="block"
 		if(timeout) {	setTimeout(()=>this.hide(), timeout) }
+		this.shadowRoot.getElementById("close").focus()
 	}
 	hide() {
 		this.shadowRoot.getElementById("main").style.display="none"
